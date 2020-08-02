1b7025b62e4649912ee6ec00ab938c5c4dfa5365
Alex Koutmos <akoutmos@gmail.com>
2020-08-02 18:02:28 -0400

    Initial commit

---
+++ lib/replug.ex
@@ -0,0 +1,18 @@
+defmodule Replug do
+  @moduledoc """
+  Documentation for `Replug`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Replug.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/replug_test.exs
@@ -0,0 +1,8 @@
+defmodule ReplugTest do
+  use ExUnit.Case
+  doctest Replug
+
+  test "greets the world" do
+    assert Replug.hello() == :world
+  end
+end
