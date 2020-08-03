de722f91c3b125b676b1838504e9046f3a1ccf39
Alex Koutmos <akoutmos@gmail.com>
2020-08-02 21:50:02 -0400

    Initial implementation for replug

--- lib/replug.ex
+++ lib/replug.ex
@@ -1,18 +1,75 @@
 defmodule Replug do
   @moduledoc """
-  Documentation for `Replug`.
+  ```
+  plug Replug,
+    plug: {Corsica, max_age: 600, expose_headers: ~w(X-Foo)},
+    opts: fn ->
+      [origins: System.get_env("VALID_ORIGINS")]
+    end
+  ```
+
+  or
+
+  ```
+  plug Replug,
+    plug: Corsica,
+    opts: fn ->
+      [
+        origins: System.get_env("VALID_ORIGINS"),
+        max_age: 600,
+        expose_headers: ~w(X-Foo)
+      ]
+    end
+  ```
+
   """
 
-  @doc """
-  Hello world.
+  @behaviour Plug
 
-  ## Examples
+  @impl true
+  def init(opts) do
+    plug =
+      case Keyword.get(opts, :plug) do
+        {module, opts} when is_atom(module) and is_list(opts) ->
+          {module, opts}
 
-      iex> Replug.hello()
-      :world
+        module when is_atom(module) ->
+          {module, :only_dynamic_opts}
 
-  """
-  def hello do
-    :world
+        nil ->
+          raise("Replug requires a :plug entry")
+      end
+
+    %{
+      plug: plug,
+      opts: Keyword.get(opts, :opts) || raise("Replug requires a :opts entry")
+    }
+  end
+
+  @impl true
+  def call(conn, %{plug: {module, :only_dynamic_opts}, opts: opts_function}) do
+    opts =
+      opts_function.()
+      |> module.init()
+
+    module.call(conn, opts)
+  end
+
+  def call(conn, %{plug: {module, static_opts}, opts: opts_function}) do
+    opts =
+      static_opts
+      |> merge_opts(opts_function.())
+      |> module.init()
+
+    module.call(conn, opts)
+  end
+
+  defp merge_opts(static_opts, dynamic_opts)
+       when is_list(static_opts) and is_list(dynamic_opts) do
+    Keyword.merge(static_opts, dynamic_opts)
+  end
+
+  defp merge_opts(static_opts, dynamic_opts) when is_map(static_opts) and is_map(dynamic_opts) do
+    Map.merge(static_opts, dynamic_opts)
   end
 end
--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule Replug.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:plug, "~> 1.8"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.12", only: :test, runtime: false}
     ]
   end
 end
